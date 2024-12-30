e0536cadbce89c14e163ea55efb6db909579ecaa
AHEAD94 <ulysses94@naver.com>
2024-12-30 22:09:50 +0900

    feat: 날씨 저장을 위한 버튼 추가

---
+++ ios/how-is-outside/how-is-outside/Views/Management/WeatherSaveView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherSaveView.swift
+//  how-is-outside
+//
+//  Created by Ryu Dae-ha on 12/30/24.
+//
+
+import SwiftUI
+
+struct WeatherSaveView: View {
+    var body: some View {
+        Text("현재날씨 저장화면")
+    }
+}
+
+#Preview {
+    WeatherSaveView()
+}
