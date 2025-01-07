13d6e3a5fb9f37e2a5b5a28d761c9ac067dba4e9
AHEAD94 <ulysses94@naver.com>
2025-01-07 22:25:27 +0900

    feat: 체감정보 리스트 정보에 날짜와 시간 추가

--- ios/how-is-outside/how-is-outside/Views/History/HistoryRow.swift
+++ ios/how-is-outside/how-is-outside/Views/History/HistoryRow.swift
@@ -12,28 +12,39 @@ struct HistoryRow: View {
     
     var body: some View {
         HStack {
+            VStack {
+                Text(feedback.date)
+                Text(feedback.time)
+            }
+            .frame(maxWidth: .infinity)
+            .lineLimit(1)
+            .minimumScaleFactor(0.1)
+            Spacer()
             Text(feedback.user_rating)
                 .frame(maxWidth: .infinity)
                 .lineLimit(1)
+                .minimumScaleFactor(0.1)
                 .padding(.vertical, 8)
             Spacer()
             Text(feedback.description)
                 .frame(maxWidth: .infinity)
-                .lineLimit(2)
-                .multilineTextAlignment(.center)
+                .lineLimit(1)
                 .minimumScaleFactor(0.1)
             Spacer()
             Text(feedback.temperature)
                 .frame(maxWidth: .infinity)
                 .lineLimit(1)
+                .minimumScaleFactor(0.1)
             Spacer()
             Text(feedback.humidity)
                 .frame(maxWidth: .infinity)
                 .lineLimit(1)
+                .minimumScaleFactor(0.1)
             Spacer()
             Text(feedback.wind)
                 .frame(maxWidth: .infinity)
                 .lineLimit(1)
+                .minimumScaleFactor(0.1)
         }
     }
 }
@@ -43,6 +54,5 @@ struct HistoryRow: View {
     return Group {
         HistoryRow(feedback: feedbacks[0])
         HistoryRow(feedback: feedbacks[1])
-        HistoryRow(feedback: feedbacks[13])
     }
 }
